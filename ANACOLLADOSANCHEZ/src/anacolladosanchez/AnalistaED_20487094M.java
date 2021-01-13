ad5c1cb95ae1d2b1920174b87404f27864f59d88
Ana Collado <anacolladosanchez@gmail.com>
2021-01-13 19:59:04 +0100

    código sin javadoc DNI 20487094M

---
+++ ANACOLLADOSANCHEZ/src/anacolladosanchez/AnalistaED_20487094M.java
@@ -0,0 +1,34 @@
+package anacolladosanchez;
+
+import java.util.*;
+
+public class AnalistaED_20487094M {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public AnalistaED_20487094M(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA        
+        AnalistaED_20487094M emp = new AnalistaED_20487094M("ANA COLLADO", "20487094M");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto 
+		+ " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+                + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+
+}
